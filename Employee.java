02d3693874782333790834955aa260e5d1006406
macandsheesh <117171089+macandsheesh@users.noreply.github.com>
2022-11-01 13:47:38 +0000

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,27 @@
+package database;
+
+public class Employee {
+
+	private int employeeId;
+	private String employeeName;
+	private double grossSalary;
+	private double netSalary;
+	
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName=employeeName;
+	}
+	public void setgrossSalary(double grossSalary) {
+		this.grossSalary=grossSalary;
+	}
+	public void setnetSalary(double netSalary) {
+		this.netSalary=netSalary;
+	}
+	
+	
+	}
+	
+
+}
