18e51894585a4671819d7d36aae266830ef47abe
macandsheesh <117171089+macandsheesh@users.noreply.github.com>
2022-11-01 11:59:56 +0000

    Add files via upload

---
+++ Payroll.java
@@ -0,0 +1,23 @@
+
+public class Payroll{ 
+
+	public Payroll() {
+		setSalary();
+		setTaxRate();
+		set 
+		
+		
+	}
+
+	private void setTaxRate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void setSalary() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	}
